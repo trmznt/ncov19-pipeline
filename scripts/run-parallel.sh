60a3cff65c20e376bef8ef2e7da4275d8f1a1515
Anto - alrescha <anto@eijkman.go.id>
2021-05-12 10:45:25 +0700

    add --eta argument when starting parallel

--- scripts/run-parallel.sh
+++ scripts/run-parallel.sh
@@ -4,5 +4,5 @@ _script="$(readlink -f ${BASH_SOURCE[0]})"
 ## Delete last component from $_script ##
 _mydir="$(dirname $_script)"
 
-parallel --sshlogin 4/cu-4,4/cu-5 --workdir $PWD/{} ${_mydir}/run-pipeline.sh all ::: `ls`
+parallel --eta --sshlogin 8/cu-4,8/cu-5 --workdir $PWD/{} ${_mydir}/run-pipeline.sh all ::: `ls`
 
