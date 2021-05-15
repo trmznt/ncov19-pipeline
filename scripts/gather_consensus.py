207a00adf5051e040e80468cd7d73aa5914f6ae1
Anto - alrescha <anto@eijkman.go.id>
2021-05-15 16:23:09 +0700

    gather stat file as well

--- scripts/gather_consensus.py
+++ scripts/gather_consensus.py
@@ -6,6 +6,7 @@ from seqpy import cout, cerr, cexit
 from seqpy.cmds import arg_parser
 from seqpy.core.bioio import load, save, multisequence, biosequence
 from seqpy.core.funcs import profiles
+import os
 
 
 def init_argparser(p=None):
@@ -14,9 +15,10 @@ def init_argparser(p=None):
         p = arg_parser('gather_consensus.py - gather consensus')
 
     p.add_argument('--add', default=None)
-    p.add_argument('-o', '--outfile', default='consensus.fas')
-    p.add_argument('-i', '--infile', default='/cons/trimmed.fa')
-    p.add_argument('indirs', nargs='+')
+    p.add_argument('-o', '--outdir', default='')
+    p.add_argument('-c', '--consfile', default='cons/consensus.fa')
+    p.add_argument('-s', '--statfile', default='logs/stats.tsv')
+    p.add_argument('indir')
 
     return p
 
@@ -27,20 +29,49 @@ def main( args ):
 
 def gather_consensus( args ):
 
+    # set output directory
+    args.outdir = args.indir + '-results' if not args.outdir else args.outdir
+
     # open input file
     cons = multisequence()
+    header = None
+    stat_lines = []
 
     if args.add:
         seqs = load(args.add)
         cons.extend( seqs )
 
-    for indir in args.indirs:
+    for indir in sorted(os.listdir(args.indir)):
+
+        seqpath = os.path.join(args.indir, indir, args.consfile)
+        print(args.indir, indir, args.consfile, seqpath)
         try:
-            seqs = load(indir + args.infile)
+            seqs = load(seqpath)
         except FileNotFoundError:
-            cerr('[WARN: no such file: %s]' % (indir + args.infile) )
+            cerr('[WARN: no such file: %s]' % (seqpath) )
             continue
 
         cons.append( biosequence(indir, seqs[0].seq))
 
-    save( cons, args.outfile )
+        statpath = os.path.join(args.indir, indir, args.statfile)
+        with open(statpath) as fin:
+            lines = fin.read().split('\n')
+            if not header:
+                header = lines[0].strip()
+            stat_lines.append( lines[1].strip() )
+
+    try:
+        os.mkdir(args.outdir)
+    except:
+        pass
+
+    save( cons, os.path.join(args.outdir, 'consensus.fas' ) )
+    with open( os.path.join(args.outdir, 'stats.tsv'), 'w') as fout:
+        fout.write(header)
+        fout.write('\n')
+        fout.write('\n'.join(stat_lines))
+
+    cerr(f'[Writing results to directory {args.outdir}]')
+
+
+
