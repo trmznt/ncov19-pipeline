5ae0aa1931cec6d0c1000228a64b5f7a4ca370b4
Anto - alrescha <anto@eijkman.go.id>
2021-09-28 10:39:53 +0700

    add combine-reports.py

---
+++ scripts/combine-reports.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+
+
+def parse_args():
+    p = argparse.ArgumentParser(prog='combine-report.py')
+    p.add_argument('-o', '--outfile', default='full-reports.csv')
+    p.add_argument('--outpass', default='submission-pass.tsv')
+    p.add_argument('--outfailed', default='submission-failed.tsv')
+    p.add_argument('-m', '--metafile')
+    p.add_argument('-s', '--statfile', default='stats.tsv')
+    p.add_argument('-l', '--lineagefile', default='lineage_report.csv')
+    return p.parse_args()
+
+
+def combine_report(args):
+
+    df_metadata = pd.read_table(args.metafile, sep=',')
+
+    df_stat = pd.read_table(args.statfile, sep='\t')
+    df = df_metadata.join(df_stat.set_index('SAMPLE'), on='fn')
+
+    df_lineage = pd.read_table(args.lineagefile, sep=',')
+    df = df.join(df_lineage.set_index('taxon'), on='fn')
+
+    df.to_csv(args.outfile, index=False, sep=',')
+
+    pass_filter = (df['N_BASE'] < 1000) & (df['LENGTH'] > 28000) & (df['ambiguity_score'] > 0.8)
+    df_pass = df[pass_filter]
+    df_pass.to_csv(args.outpass, index=False, sep='\t')
+    df_failed = df[~ pass_filter]
+    df_failed.to_csv(args.outfailed, index=False, sep='\t')
+    print(f'Sequence passed QC: {len(df_pass)}')
+
+
+def main():
+    args = parse_args()
+    combine_report(args)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
