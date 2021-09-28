e79f56fdb577305990bde48245c2792ba8ed0a4e
Anto - alrescha <anto@eijkman.go.id>
2021-09-28 08:00:25 +0700

    add progress indicator and covv_seq_technology option

--- scripts/prepare-submission.py
+++ scripts/prepare-submission.py
@@ -28,10 +28,12 @@ def prepare_submission(args):
     #import IPython; IPython.embed()
 
     # open infile tsv
+    cerr(f'[Reading infile {args.infile}]')
     submission_df = pd.read_table(args.infile, sep='\t')
     submission_df['SAMPLE'] = submission_df['SAMPLE'].astype('str')
 
     # open sequence file
+    cerr(f'[Reading sequence file {args.seqfile}]')
     mseq = bioio.load( args.seqfile )
     mseq_keys = {}
     for i in range(len(mseq)):
@@ -48,10 +50,12 @@ def prepare_submission(args):
         if sample_id not in mseq_keys:
             continue
 
+        cerr(f'[Preparing sample {sample_id}]')
         # set coverage
         # import IPython; IPython.embed()
         metadata_df.at[sample_id, 'covv_coverage'] = s['AVGDEPTH']
         metadata_df.at[sample_id, 'fn'] = out_fasta
+        metadata_df.at[sample_id, 'covv_seq_technology'] = args.covv_seq_technology
         metadata_df.at[sample_id, 'covv_assembly_method'] = args.covv_assembly_method
 
         # set sequence name
@@ -59,6 +63,7 @@ def prepare_submission(args):
         mseq[idx].label = r['covv_virus_name']
         mseq[idx].seq = mseq[idx].seq.strip(b'-')
         used.append(sample_id)
+        cerr(f'[Finish preparing  sample {sample_id}]')
 
     # remove unused metadata
     metadata_df = metadata_df.loc[ used ]
@@ -73,6 +78,7 @@ def init_argparser():
     p = arg_parser('prepare GISAID submission files')
 
     p.add_argument('--covv_assembly_method', default='custom minimap2 + iVar pipeline')
+    p.add_argument('--covv_seq_technology', default='Illumina NextSeq 550')
     p.add_argument('--metafile', required=True)
     p.add_argument('--seqfile', required=True)
     p.add_argument('--outprefix', required=True)
